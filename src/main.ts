a76dc45a1aa6b45cb3609441a3a72a58e86223bc
NDenheim <nicole.denheim@hotmail.co.uk>
2023-08-09 12:47:47 +0100

    Function works for input field rather than manual

--- src/main.ts
+++ src/main.ts
@@ -19,20 +19,39 @@ if (!englishInput || !morseOutput || !englishSubmit) {
 // let userInput = englishInput.value;
 // console.log(userInput);
 
-const translateEnglishToMorse = (input: string) => {
-  const splitWord = input.toLowerCase().split("");
+function returnText(event: Event) {
+  event.preventDefault();
+  let userInput = englishInput.value;
+
+  const splitWord = userInput.toLowerCase().split("");
   // return splitWord; // ['h', 'e', 'y', ' ', 't', 'h', 'e', 'r', 'e']
   const convertedArr = splitWord.map((key) => {
     // console.log(englishToMorse[key]);
     return englishToMorse[key];
   });
-  return convertedArr.join(" ");
+  // return convertedArr.join(" ");
   // morseOutput.value += convertedArr.join(" ");
-  // console.log(convertedArr.join(" "));
-};
+  const finalOutput = convertedArr.join(" ");
+  morseOutput.value = finalOutput;
+}
+
+englishSubmit.addEventListener("click", returnText);
+// morseOutput.value = translateEnglishToMorse;
+
+// const translateEnglishToMorse = (input: string) => {
+//   const splitWord = input.toLowerCase().split("");
+//   // return splitWord; // ['h', 'e', 'y', ' ', 't', 'h', 'e', 'r', 'e']
+//   const convertedArr = splitWord.map((key) => {
+//     // console.log(englishToMorse[key]);
+//     return englishToMorse[key];
+//   });
+//   return convertedArr.join(" ");
+//   // morseOutput.value += convertedArr.join(" ");
+//   // console.log(convertedArr.join(" "));
+// };
 
 // morseOutput.value = translateEnglishToMorse("Hey there");
-morseOutput.value = translateEnglishToMorse(englishInput.textContent);
+// morseOutput.value = translateEnglishToMorse();
 
 // for (const key in englishToMorse) {
 //   console.log(key);
