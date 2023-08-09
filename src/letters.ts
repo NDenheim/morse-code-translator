2e539ae0a88b3fb034786ffb290ab452497c9592
NDenheim <nicole.denheim@hotmail.co.uk>
2023-08-09 12:03:59 +0100

    Creating object containing letter translation

---
+++ src/letters.ts
@@ -0,0 +1,37 @@
+export type letterPair = {
+  [key: string]: string;
+};
+
+// export const translation: letterPair = {
+//   english: "a",
+//   morse: ".-",
+// };
+
+export const englishToMorse: letterPair = {
+  a: ".-",
+  b: "-...",
+  c: "-.-.",
+  d: "-..",
+  e: ".",
+  f: "..-.",
+  g: "--.",
+  h: "....",
+  i: "..",
+  j: ".---",
+  k: "-.-",
+  l: ".-..",
+  m: "--",
+  n: "-.",
+  o: "---",
+  p: ".--.",
+  q: "--.-",
+  r: ".-.",
+  s: "...",
+  t: "-",
+  u: "..-",
+  v: "...-",
+  w: ".--",
+  x: "-..-",
+  y: "-.--",
+  z: "--..",
+};
